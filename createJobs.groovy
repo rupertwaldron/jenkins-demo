587c4c5b12aa0a1ec797e8ab5ca25c0ed2a9bf16
rupertwaldron <rupert.waldron@yahoo.co.uk>
2023-07-31 15:20:51 +0100

    Theme-park-updated

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,19 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+
+pipelineJob('theme-park-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/rupertwaldron/spring-boot-api-example'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
+
